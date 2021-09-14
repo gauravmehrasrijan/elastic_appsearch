da2386233ac1ffaab8e78bf01d191151ff43ca22
Ridhima Abrol <ridhima.abrol@srijan.net>
2021-09-14 12:52:06 +0530

    Adding logger to elastic search index

--- src/Entity/Engine.php
+++ src/Entity/Engine.php
@@ -281,6 +281,8 @@ class Engine extends ConfigEntityBase implements EngineInterface {
    * {@inheritdoc}
    */
   public function indexDocuments($documents) {
+    $path = \Drupal::service('path.current')->getPath();
+    \Drupal::logger('elastic_appsearch')->notice('Indexed ' . $this->id() . ' from' . $path);
     return $this->getClient()->indexDocuments($this->id(), $documents);
   }
 
